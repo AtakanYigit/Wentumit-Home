774fb78e9401fb93014a747fa865b64302aeb6ba
Atakan Yiğit Çengeloğlu <atakanyigit85@gmail.com>
2024-08-30 17:45:37 +0300

    - Ideas Responsieness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -60,59 +60,61 @@ const Header = () =>{
     }, []);
 
     return (
-        <div className = "header">
-            <div className = "headerContainer">
-                <div className = "changingText">
-                    <h1>
-                        <span className = "start">Can’t Decide&nbsp;</span>
-                        <span className = "word">When</span>
-                        <span className = "word">Where</span>
-                        <span className = "word">Time</span>
-                        <br/>To Meet?
-                    </h1>
-                </div>
-                
-                <div className = "infoContainer">
-                    <p className = "infoText">
-                        No more back and forth messages. Just open up a poll and let your friends swipe to vote on the best time to meet. <span>Wentumit will automatically find the best time / place / day for everyone. So you can spend less time planning and more time meeting.</span>
-                    </p>
-                    <p className = "startUsingToday">
-                        Start using Wentumit today!
-                    </p>
-                    <div className = "startUsingContainer">
-                        <a href = "https://play.google.com/store/apps/details?id=com.wentumit.www.twa&hl=en" target = "_blank" className = "googlePlayContainer">
-                            <div className = "qrContainer">
-                                <img className = "qr noSelect" src = {QR} alt = "qr" />
-                            </div>
-                            <div className = "googlePlayContainerInner">
-                                <img src = {GooglePlayLogo} alt = "" />
-                                <p>Google Play</p>
-                            </div>
-                        </a>
-
-                        <p className = "or">or</p>
-
-                        <a className = "onYourBrowserButton" href = "https://www.wentumit.com/">
-                            <span className = "iconWrapper">
-                                <svg width = "10" className = "iconSvg" xmlns = "http://www.w3.org/2000/svg" fill = "none" viewBox = "0 0 14 15">
-                                    <path fill = "#000000" d = "M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
-                                </svg>
-                                
-                                <svg className = "iconSvg  iconSvg--copy" xmlns = "http://www.w3.org/2000/svg" width = "10" fill = "none" viewBox = "0 0 14 15">
-                                    <path fill =   "#000000" d = "M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
-                                </svg>
-                            </span>
-                            On Your Browser
-                        </a>
+        <>
+            <div className = "header">
+                <div className = "headerContainer">
+                    <div className = "changingText">
+                        <h1>
+                            <span className = "start">Can’t Decide&nbsp;</span>
+                            <span className = "word">When</span>
+                            <span className = "word">Where</span>
+                            <span className = "word">Time</span>
+                            <br/>To Meet?
+                        </h1>
                     </div>
-                    {/* <CTA/> */}
                     
-                    <p className = "completelyFree">
-                        Completely Free!
-                    </p>
+                    <div className = "infoContainer">
+                        <p className = "infoText">
+                            No more back and forth messages. Just open up a poll and let your friends swipe to vote on the best time to meet. <span>Wentumit will automatically find the best time / place / day for everyone. So you can spend less time planning and more time meeting.</span>
+                        </p>
+                        <p className = "startUsingToday">
+                            Start using Wentumit today!
+                        </p>
+                        <div className = "startUsingContainer">
+                            <a href = "https://play.google.com/store/apps/details?id=com.wentumit.www.twa&hl=en" target = "_blank" className = "googlePlayContainer">
+                                <div className = "qrContainer">
+                                    <img className = "qr noSelect" src = {QR} alt = "qr" />
+                                </div>
+                                <div className = "googlePlayContainerInner">
+                                    <img src = {GooglePlayLogo} alt = "" />
+                                    <p>Google Play</p>
+                                </div>
+                            </a>
+
+                            <p className = "or">or</p>
+
+                            <a className = "onYourBrowserButton" href = "https://www.wentumit.com/">
+                                <span className = "iconWrapper">
+                                    <svg width = "10" className = "iconSvg" xmlns = "http://www.w3.org/2000/svg" fill = "none" viewBox = "0 0 14 15">
+                                        <path fill = "#000000" d = "M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                                    </svg>
+                                    
+                                    <svg className = "iconSvg  iconSvg--copy" xmlns = "http://www.w3.org/2000/svg" width = "10" fill = "none" viewBox = "0 0 14 15">
+                                        <path fill =   "#000000" d = "M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                                    </svg>
+                                </span>
+                                On Your Browser
+                            </a>
+                        </div>
+                        {/* <CTA/> */}
+                        
+                        <p className = "completelyFree">
+                            Completely Free!
+                        </p>
+                    </div>
                 </div>
             </div>
-            
+
             <div className = "ideasContainer">
                 <div className = "idea ideaLeft idea1 noSelect">
                     <p>I'm available on Tuesday<br/>but not on Friday 1.</p>
@@ -133,7 +135,7 @@ const Header = () =>{
                     <p>I'm available on Tuesday<br/>but not on Friday 6.</p>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
